234e051d9d6bbb73af5826577a5f2f33b0239d20
JesusEMorales <jesusculajay116@gmail.com>
2026-09-23 10:24:23 -0600

    División finalizada

---
+++ division.js
@@ -0,0 +1,44 @@
+let pantalla = document.getElementById("pantalla");
+
+function Agregar(dato) {
+    pantalla.value += dato;
+}
+
+function Eliminar(){
+    pantalla.value = "";
+}
+
+function Calcular(){
+    pantalla.value = eval(pantalla.value);
+}
+
+const carruselElemento = document.querySelector(".carrusel-items");
+
+let posicionMaximaDesplazamiento = carruselElemento.scrollWidth - carruselElemento.clientWidth;
+let intervaloDesplazamiento = null;
+let pasoDesplazamiento = 1;
+
+const iniciarDesplazamiento = () => {
+  intervaloDesplazamiento = setInterval(function () {
+    carruselElemento.scrollLeft = carruselElemento.scrollLeft + pasoDesplazamiento;
+    if (carruselElemento.scrollLeft === posicionMaximaDesplazamiento) {
+      pasoDesplazamiento = pasoDesplazamiento * -1;
+    } else if (carruselElemento.scrollLeft === 0) {
+      pasoDesplazamiento = pasoDesplazamiento * -1;
+    }
+  }, 50);
+};
+
+const detenerDesplazamiento = () => {
+  clearInterval(intervaloDesplazamiento);
+};
+
+carruselElemento.addEventListener("mouseover", () => {
+  detenerDesplazamiento();
+});
+
+carruselElemento.addEventListener("mouseout", () => {
+  iniciarDesplazamiento();
+});
+
+iniciarDesplazamiento();
